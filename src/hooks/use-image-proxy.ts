55c32ff83232964ca3f913c840042fab747aad9b
Vladislav Adamets <adametsderschopfer@yandex.ru>
2026-01-28 16:29:41 +0200

    add prefix for gifts images

--- src/hooks/use-image-proxy.ts
+++ src/hooks/use-image-proxy.ts
@@ -1,9 +1,11 @@
 import {useMemo} from 'react';
+import {EDataSourceName} from "../../types";
 
 export function useImageProxy(src) {
   return useMemo(() => {
-      if (src.includes('thumbnails/')) {
-        return `/api/gifts-image?path=${encodeURIComponent(src)}`;
+    const giftsIdString = `_${EDataSourceName.Gifts}_`
+      if (src.includes(giftsIdString)) {
+        return `/api/gifts-image?path=${encodeURIComponent(src.replace(giftsIdString, ''))}`;
       }
 
       return src;
